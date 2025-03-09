09fcf75f3ef2ad0089a1382d6068dda8c35d9a7c
r1lUnreal <150723650+r1lUnreal@users.noreply.github.com>
2025-03-09 16:26:07 +0300

    update 09.03.2025

--- Program.cs
+++ Program.cs
@@ -14,6 +14,10 @@ namespace ConsoleApp
             {
                 Console.WriteLine($"Имя директории: {directoryInfo.Name}");
 
+                //? Размер каталога
+                long folderSize = CalculateFolderSize(directoryInfo);
+                Console.WriteLine($"Размер каталога: {folderSize} байт");
+
                 //? Список файлов в каталоге
                 FileInfo[] files = directoryInfo.GetFiles();
                 Console.WriteLine($"Файлы в каталоге: {files.Length}");
@@ -22,17 +26,45 @@ namespace ConsoleApp
                     Console.WriteLine($"\tФайл: {file.Name}, Размер: {file.Length} байт");
                 }
 
+                //? Список папок в каталоге
+                DirectoryInfo[] folders = directoryInfo.GetDirectories();
+                Console.WriteLine($"Папки в каталоге: {folders.Length}");
+                foreach (var dir in folders)
+                {
+                    Console.WriteLine($"\tПапка: {dir.Name}");
+                }
+
                 //? Получение спика каталогов
                 DirectoryInfo[] subDirectories = directoryInfo.GetDirectories();
                 Console.WriteLine($"Подкаталоги: {directoryInfo.Name}");
 
                 //? Удаление файлов
-                Directory.Delete(@"C:\Users\rilUnreal\Documents\file is project" + @"\test folder 1", true);
+                //*Directory.Delete(@"C:\Users\rilUnreal\Documents\file is project" + @"\test folder 1", true);
             }
             else
             {
                 Console.WriteLine("Такого файла не существует");
             }
+        }
+
+        //? Функция для расчёта размера каталога
+        public static long CalculateFolderSize(DirectoryInfo directory)
+        {
+            long size = 0;
+
+            //? Суммирование всех файлов в каталоге
+            foreach (FileInfo file in directory.GetFiles())
+            {
+                size += file.Length;
+            }
+
+            //? Рекурсивное суммирование всех подкаталогов
+            foreach (DirectoryInfo dir in directory.GetDirectories())
+            {
+                size += CalculateFolderSize(dir);
+            }
+
+            return size;
         }
     }
 }
